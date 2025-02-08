12f53b51a52995f2ccf90bc9bf0762e6ef2c612b
NET_14_NguyenLeMinh <minhvb123456@gmail.com>
2025-02-08 08:52:45 +0700

    test

--- src/GaraMS.API/Controllers/WeatherForecastController.cs
+++ src/GaraMS.API/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace GaraMS.API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherFoadadadrecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
