45b6f67473cbbab238b71c7756c278f1f589b452
trungdq3305 <youremail@example.com>
2025-03-29 14:04:08 +0700

    manager shift

--- src/GaraMS.API/Controllers/EmployeeController.cs
+++ src/GaraMS.API/Controllers/EmployeeController.cs
@@ -1,7 +1,11 @@
-﻿using GaraMS.Data.ViewModels.EmployeeModel;
+﻿using GaraMS.Data.Models;
+using GaraMS.Data.ViewModels.EmployeeModel;
+using GaraMS.Service.Services.AccountService;
 using GaraMS.Service.Services.EmployeeService;
+using GaraMS.Service.Services.TokenService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GaraMS.API.Controllers
 {
@@ -11,10 +15,15 @@ namespace GaraMS.API.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeService _employeeService;
-
-        public EmployeeController(IEmployeeService employeeService)
+        private readonly GaraManagementSystemContext _context;
+        private readonly ITokenService _token;
+        private readonly IAccountService _accountService;
+        public EmployeeController(IEmployeeService employeeService, GaraManagementSystemContext context, ITokenService token, IAccountService accountService)
         {
             _employeeService = employeeService;
+            _context = context;
+            _token = token;
+            _accountService = accountService;
         }
 
         [HttpGet]
@@ -96,5 +105,53 @@ namespace GaraMS.API.Controllers
             var result = await _employeeService.GetAllSpeAsync();
             return Ok(result);
         }
+
+        //thêm ca làm cho nhân viên
+        [HttpPost("Add-Shift-To-Employee")]
+        public async Task<IActionResult> AddShiftToEmployee([FromQuery] int employeeId, [FromQuery] int shiftId, [FromQuery] int month)
+        {
+            var token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var decodeModel = _token.decode(token);
+            var isValidRole = _accountService.IsValidRole(decodeModel.role, new List<int>() { 2, 3 });
+            var useid = Convert.ToInt32(decodeModel.userid);
+            if (!isValidRole)
+            {
+                return Unauthorized("Bạn không có quyền truy cập.");
+            }
+
+            
+            var a = await _context.EmployeeShifts.FirstOrDefaultAsync(x => x.ShiftId == shiftId && x.EmployeeId == employeeId && x.Month == month);
+            if (a != null)
+            {
+                return BadRequest(new { status = "Error", message = "Shift duplicate" });
+            }
+            if(month < 1 || month > 12)
+            {
+                return BadRequest(new { status = "Error", message = "invalid month" });
+            }
+            var es = new EmployeeShift
+            {
+                EmployeeId = employeeId,
+                ShiftId = shiftId,
+                Month = month
+            };
+            _context.EmployeeShifts.Add(es);
+            await _context.SaveChangesAsync();
+            return Ok(es);
+        }
+
+        [HttpGet("shift")]
+        public async Task<IActionResult> GetShifts()
+        {
+            var token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var decodeModel = _token.decode(token);
+            var isValidRole = _accountService.IsValidRole(decodeModel.role, new List<int>() { 2, 3 });
+            var useid = Convert.ToInt32(decodeModel.userid);
+            var shifts = await _context.Shifts
+                .ToListAsync();
+
+            return StatusCode(200, shifts);
+        }
+
     }
 }
